9b96894dcda615662d1fff1895a31b76403fcadd
NavilaAkhsani <navilaakhsanilfitri@gmail.com>
2019-02-06 19:47:18 +0700

    marah

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ session_start();
 <?php include("template/header.php"); ?>
 
 	<h2>User CRUD</h2>
-    <h2>Hello <?= $_SESSION['email'] ?></h2>
+    <h2>Hello marah<?= $_SESSION['email'] ?></h2>
     <h2>Sebagai <?= $_SESSION['role'] ?></h2>
 	<p>Halaman tentang manajemen pengguna / user </p>
 	<ul>

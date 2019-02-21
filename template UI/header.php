d0376835a2e54494426461b89f07f07095d04eed
Aditya Pratama <adityapratama@elcapitans-MacBook-Pro.local>
2019-02-21 23:26:10 +0700

    21-02-19

--- template UI/header.php
+++ template UI/header.php
@@ -4,13 +4,13 @@
 	<meta charset="utf-8">
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Sample App</title>
+	<title>OPTAN</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
 </head>
 <body>
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-		<a class="navbar-brand" href="/sample-app">Sample App</a>
+		<a class="navbar-brand" href="/sample-app">OPTAN</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>

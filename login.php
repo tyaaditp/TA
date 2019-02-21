0acddaf5da2bf88a3fee99f42856ec4bf7030ccc
Aditya Pratama <adityapratama@elcapitans-MacBook-Pro.local>
2019-02-21 21:08:11 +0700

    21-02-19

--- login.php
+++ login.php
@@ -1,77 +1,21 @@
 <?php
-session_start();
+	session_start();
+	if(isset($_SESSION['username'])) {
+		header('Location: /sample-app/');
+		die;
+	}
 ?>
-
 <?php include("template/header.php"); ?>
-
-	<h2>User CRUD</h2>
-    <h2>Hello <?= $_SESSION['email'] ?></h2>
-    <h2>Sebagai <?= $_SESSION['role'] ?></h2>
-	<p>Halaman tentang manajemen pengguna / user </p>
-	<ul>
-		<li><a href="user/index.php">List</a></li>
-		<li><a href="user/create.php">Create</a></li>
-	</ul>
-    <div>
-		<canvas id="cnvs" width="800" height="500"></canvas>
+<h2>Login</h2>
+<form action="/sample-app/auth/login.php" method='POST'>
+	<div class="form-group">
+		<label for="inputEmail">Email address</label>
+		<input type="email" class="form-control" id="inputEmail" name="email">
 	</div>
-	<div>
-		<input type="color" id="color">
+	<div class="form-group">
+		<label for="inputPassword">Password</label>
+		<input type="password" class="form-control" id="inputPassword" name="password">
 	</div>
-</div>
-<?php include("template/footer.php"); ?>
-
-<script>
-(function (w, d) {
-	"use strict";
-	var action = "up",
-		canvas = d.querySelector("#cnvs"),
-		color = d.querySelector("#color"),
-		body = d.querySelector("body"),
-		ctx = canvas.getContext("2d"),
-		offset = 1000,
-		points = [],
-		bufer = ctx.getImageData(0, 0, canvas.width,canvas.height);
-	ctx.lineWidth = 4;
-	ctx.shadowColor = "#000000";
-	ctx.shadowBlur = 1;
-	ctx.shadowOffsetX = -offset;
-	body.addEventListener("mousedown", function(e){
-		action = "down";
-		points.push([e.pageX, e.pageY]);
-	});
-	body.addEventListener("mousemove", function(e){
-		if (action === "down") {
-			ctx.putImageData(bufer, 0, 0);
-			points.push([e.pageX,e.pageY]);
-			ctx.beginPath();
-			ctx.moveTo(points[0][0]+offset, points[0][1]);
-			for (var i = 1; i < points.length; i++){
-				ctx.lineTo(points[i][0]+offset, points[i][1]);
-			}
-			ctx.stroke();
-		}
-	});
-	body.addEventListener("mouseup", function(){
-		action = "up";
-		points = [];
-		bufer = ctx.getImageData(0, 0, canvas.width,canvas.height);
-	});
-	color.addEventListener("change", function(e){
-		ctx.shadowColor = e.target.value;
-	});
-}(window, document));
-</script>
-
-<!DOCTYPE html>
-<html>
-<body>
-
-<form action="upload.php" method="post" enctype="multipart/form-data">
-    Pilih gambar retina untuk diunggah:
-    <input type="file" name="fileToUpload" id="fileToUpload">
-    <input type="submit" value="Upload Gambar" name="submit">
+	<button type="submit" class="btn btn-primary">Login</button>
 </form>
-
-</body>
-</html>
+<?php include("template/footer.php"); ?>

7e647d5a7674c7f27c9658ccc6ec63a2f6b222ac
NavilaAkhsani <navilaakhsanilfitri@gmail.com>
2019-04-09 01:10:43 +0700

    admin php remake

--- admin.php
+++ admin.php
@@ -1,33 +1,71 @@
 <!DOCTYPE html>
 <html>
 <head>
- <title>Table with database</title>
- <style>
-  table {
-   border-collapse: collapse;
-   width: 100%;
-   color: #588c7e;
-   font-family: monospace;
-   font-size: 25px;
-   text-align: left;
-     } 
-  th {
-   background-color: #588c7e;
-   color: white;
-    }
-  tr:nth-child(even) {background-color: #f2f2f2}
- </style>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Admin Page</title>
+    <style>
+        table {
+        border-collapse: collapse;
+        width: 100%;
+        color: #231e11;
+        font-family: Helvetica, sans-serif;
+        font-size: 15px;
+        text-align: left;
+        } 
+        .tabel {
+        margin-left: 30px;
+        margin-right: 30px;
+        }
+        th {
+        background-color: #231e11;
+        color: white;
+        }
+        tr:nth-child(even) {background-color: #f2f2f2}
+    </style>
 </head>
 <body>
- <table>
- <tr>
-  <th>Id</th> 
-  <th>Nama</th>
-  <th>Email</th>
-  <th>Username</th> 
-  <th>Password</th>
-  <th>Role</th>
- </tr>
+ <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-warning">
+    <div class="container">
+        <a class="navbar-brand font-weight-bold page-scroll" href="#home" style="font-size:150%">OPTAN</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link page-scroll" href="#home">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="optan.html" tabindex="-1">Logout</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-secondary my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </div>
+ </nav>
+ <section class="mt-5 pt-5 text-center">
+    <h2>User Data</h2>
+    <div class="tabel">
+        <table>
+            <tr>
+                <th>Id</th> 
+                <th>Nama</th>
+                <th>Email</th>
+                <th>Username</th> 
+                <th>Password</th>
+                <th>Role</th>
+            </tr>
+
  <?php
 $conn = mysqli_connect("localhost", "root", "", "ta_anotasi");
   // Check connection
@@ -46,6 +84,30 @@ echo "</table>";
 } else { echo "0 results"; }
 $conn->close();
 ?>
-</table>
+        </table>
+    </div>
+ </section>
+
+
+    <!-- Footer -->
+    <footer class="bg-dark text-white fixed-bottom">
+        <div class="container text-center">
+            <div class="row pt-2">
+                <div class="col">
+                    <p style="font-size:12px">&copy; 2019 | Built with <img src="svg/si-glyph-heart.svg" style="width:16px;
+                        height:16px;"> by Nav.</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
 </body>
 </html>

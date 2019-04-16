f38c903e885b32691477bb8c3a15ce78f20ea639
Aditya Pratama <adityapratama@elcapitans-MacBook-Pro.local>
2019-04-16 17:24:56 +0700

    gk perlu

--- saving.js
+++
@@ -1,8 +0,0 @@
-// var ceksimilarity = document.getElementById('similarity');
-// ceksimilarity.onclick = ceksave() {
-//     if ('di database ada current annotated image') {
-//         masuk ke fungsi hitung, lalu tampilkan persentase
-//     } else {
-//         alert('Please save your annotated image before check the similarity !');
-//     }
-// }

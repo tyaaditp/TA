2aa8e2d5c092a34eec134c27565ddaca2b657e1a
Aditya Pratama <adityapratama@elcapitans-MacBook-Pro.local>
2019-04-25 00:24:41 +0700

    fav icon

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
+	<link rel="shortcut icon" type="image/png" href="/logooptan.png"/>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">

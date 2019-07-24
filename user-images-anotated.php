e58513e6d0e65baac173c30e076979628ce8444d
Aditya Pratama <adityapratama@elcapitans-MacBook-Pro.local>
2019-07-24 20:32:30 +0700

    imge anotated on path3

--- user-images-anotated.php
+++ user-images-anotated.php
@@ -5,19 +5,24 @@ require('./config.php');
 $user_id = $_POST['user_id'];
 $parent_id = $_POST['parent_id'];
 $image_ori = $_POST['image_ori'];
+$image_anotated = $_POST['image_ano'];
 $query = "SELECT image_original.id as 'original_id', image_original.user_id as 'original_user_id', image_original.image as 'original_image', image_anotated.id as 'anotated_id', image_anotated.user_id as 'anotated_user_id', image_anotated.image_id as 'anotated_reference', image_anotated.image as 'anotated_image', image_anotated.analisis as 'anotated_analisis'  from image_original INNER JOIN image_anotated ON image_original.id=image_anotated.image_id AND image_anotated.user_id = " . $user_id . " WHERE image_original.image = '/uploads/".$image_ori."'" ;
 
 $sql2 = mysqli_query($link, $query);
 if(mysqli_num_rows($sql2)>0) {
     while ($row2 = mysqli_fetch_assoc($sql2)) {
-        $querySelectFirstAnotated = 'select * from image_anotated where image_id='.$row2['anotated_reference'].' and user_id='.$row2['original_user_id'].' limit 1';
+        $querySelectFirstAnotated = "select * from image_anotated where image='".$image_anotated."' and user_id=".$row2['original_user_id']." limit 1";
             //echo $querySelectFirstAnotated;
             $sql3 = mysqli_query($link, $querySelectFirstAnotated);
+            
             if(mysqli_num_rows($sql3) == 1) {
+                //
                 $rowOfFirstAnotated = mysqli_fetch_array($sql3);
-                $image1 = $rowOfFirstAnotated['image'];
-                $analisis1 = $rowOfFirstAnotated['analisis'];
-                $image2 = $row2['anotated_image'];
+                $image2 = $rowOfFirstAnotated['image'];
+                $analisis2 = $rowOfFirstAnotated['analisis'];
+                
+                //
+                $image1 = $row2['anotated_image'];
                 $analisis2 = $row2['anotated_analisis'];
                 $style =     'display: inline-block; max-width: 98%; height: auto; width: 30%; margin: 1%;';
                 $htmlimage = '<img style="'. $style . '" id="img" src="'.$row2['anotated_image'].'" alt="gambar origninal" width="200"/>' ;
